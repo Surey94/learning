234f36553dd1993f538d592fad9288bd73f3232f
SURESH <k.sureshbabu1007@gmail.com>
2024-03-19 15:19:47 +0530

    practice

---
+++ t1.java
@@ -0,0 +1,16 @@
+
+public class t1{
+int a =4;
+int b = 3;
+static int c =5;
+static int d=3;
+public void addition(int a,int b){
+int c=a+b;
+System.out.println(c);
+} 
+
+public static void main(String[] args){
+t1 v = new t1();
+v.addition();
+}
+}
